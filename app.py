68def9c79c007b763ca74fa1cfedb42a9d7a3617
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-06-15 17:02:09 +0800

    enhance/add REST API prediction

--- app.py
+++ app.py
@@ -11,28 +11,31 @@ import base64
 
 app = Flask(__name__)
 
-DETECTION_URL = "/v1/object-detection/yolov5s"
+DETECTION_URL = "/inspix-models/test"
 
 
 @app.route(DETECTION_URL, methods=["POST"])
 def predict():
     if not request.method == "POST":
         return
+    base64img = request.json['base64image']
+    if True:
+        base64_bytes = base64img.encode('ascii')
+        img_data = base64.b64decode(base64_bytes)
 
-    if request.files.get("image"):
-        image_file = request.files["image"]
-        image_bytes = image_file.read()
-
-        img = Image.open(io.BytesIO(image_bytes))
+        img = Image.open(io.BytesIO(img_data))
 
         results = model(img, size=640)  # reduce size=320 for faster inference
         return results.pandas().xyxy[0].to_json(orient="records")
+    return "request invalid"
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
+    parser = argparse.ArgumentParser(
+        description="Flask API exposing YOLOv5 model")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
-
-    model = torch.hub.load("ultralytics/yolov5", "yolov5s", force_reload=True)  # force_reload to recache
-    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
+    print('downloadings')
+    model = torch.hub.load("ultralytics/yolov5", "yolov5s",
+                           force_reload=True)  # force_reload to recache
+    app.run(host="0.0.0.0", port=args.port, debug=True)
