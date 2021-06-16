4cc730fb9fc1653128b79fd060deab8f203abceb
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-06-16 12:50:08 +0800

    enhance/add socket prediction

--- app.py
+++ app.py
@@ -8,25 +8,42 @@ import torch
 from PIL import Image
 from flask import Flask, request
 import base64
+from flask_socketio import SocketIO, send, emit
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 DETECTION_URL = "/inspix-models/test"
+socket_io = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route(DETECTION_URL, methods=["POST"])
+@cross_origin()
 def predict():
-    if not request.method == "POST":
-        return
+    # if not request.method == "POST":
+    #     return
     base64img = request.json['base64image']
-    if True:
-        base64_bytes = base64img.encode('ascii')
-        img_data = base64.b64decode(base64_bytes)
 
-        img = Image.open(io.BytesIO(img_data))
+    base64_bytes = base64img.encode('ascii')
+    img_data = base64.b64decode(base64_bytes)
 
-        results = model(img, size=640)  # reduce size=320 for faster inference
-        return results.pandas().xyxy[0].to_json(orient="records")
-    return "request invalid"
+    img = Image.open(io.BytesIO(img_data))
+
+    results = model(img, size=640)  # reduce size=320 for faster inference
+    return results.pandas().xyxy[0].to_json(orient="records")
+
+
+@socket_io.on('predict')
+def handle_predict(json):
+    base64img = json['base64image']
+    filtered_base64 = base64img.split(',')[1]
+    base64_bytes = filtered_base64.encode('ascii')
+    img_data = base64.b64decode(base64_bytes)
+
+    img = Image.open(io.BytesIO(img_data))
+
+    results = model(img, size=640)
+    emit('predict-result', results.pandas().xyxy[0].to_json(orient="records"))
 
 
 if __name__ == "__main__":
@@ -37,4 +54,5 @@ if __name__ == "__main__":
     print('downloadings')
     model = torch.hub.load("ultralytics/yolov5", "yolov5s",
                            force_reload=True)  # force_reload to recache
-    app.run(host="0.0.0.0", port=args.port, debug=True)
+    socket_io.run(app, host="0.0.0.0", port=args.port, debug=True)
+    # app.run(host="0.0.0.0", port=args.port, debug=True)
--- run-venv.bat
+++ run-venv.bat
@@ -1,1 +1,3 @@
 cd C:\Users\audip
+.\venv\Scripts\activate
+cd C:\Users\audip\Documents\Projects\sota_startups\inspix\programs-02\flask-model-api
